7a252986dba228a8e8243762abcce60d8f53db90
DC-21 <cholahkuboko@gmail.com>
2023-10-28 03:49:56 +0200

    first commit

--- SimpleMiddleware/SimpleMiddleware/Program.cs
+++ SimpleMiddleware/SimpleMiddleware/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+//using Run() method to build a middleware(with lambda expression) that executes one request only
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("Hello chola");
+});
 
 app.Run();
