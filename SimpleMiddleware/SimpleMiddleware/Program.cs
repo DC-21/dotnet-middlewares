ba794bce241128e6d9662dbe251fd7efa70151cb
DC-21 <cholahkuboko@gmail.com>
2023-10-28 04:07:35 +0200

    first commit

--- SimpleMiddleware/SimpleMiddleware/Program.cs
+++ SimpleMiddleware/SimpleMiddleware/Program.cs
@@ -3,10 +3,24 @@ var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 
+//running a chain of middlewares that executes a couple middlewares.
+//comment first middleware if you want to try a middleware that ives a single response.
+app.Use(async (HttpContext context,RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("Hello");
+    await next(context);
+
+});
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("ba");
+    await next(context);
+});
 //using Run() method to build a middleware(with lambda expression) that executes one request only
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("Hello chola");
+    await context.Response.WriteAsync("dev");
 });
+
 
 app.Run();
